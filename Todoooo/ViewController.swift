7cd6dbc7a9c83da032c3ba2d60d360a2124e6c5b
Arek <arkadiusz.lapajski@hotmail.com>
2020-10-19 17:38:40 +0200

    Initial Commit

---
+++ Todoooo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoooo
+//
+//  Created by Arek Łapajski on 19/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
